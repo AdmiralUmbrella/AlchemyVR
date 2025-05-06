247b844478bbf2a266e47b88fa9f1bb9ecfed903
Umbrella420 <amwa_24@hotmail.com>
2025-05-06 00:04:55 -0500

    Modified enemies scales, Added tunneling settings

---
+++ Assets/Scripts/SaveFiles/SaveSetting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Comfort;
+
+public class SaveSetting : MonoBehaviour
+{
+    private SaveData saveData = new();
+    
+    public GameObject tunnelingVignetteController;
+
+    public string saveFilePath;
+    
+
+    private void Start()
+    {
+        saveFilePath = Application.persistentDataPath + "/PlayerData.json";
+        if (File.Exists(saveFilePath))
+        {
+            string loadPlayerSetting = File.ReadAllText(saveFilePath);
+            saveData = JsonUtility.FromJson<SaveData>(loadPlayerSetting);
+            tunnelingVignetteController.SetActive(saveData.tunneling);
+            Debug.Log("Datos cargados.");
+        }
+        else
+        {
+            Debug.Log("No se ha encontrado el archivo de datos.");
+        }
+    }
+
+    public void SaveToFile()
+    {
+        saveData.tunneling = tunnelingVignetteController.activeSelf;
+        
+        string savePlayerSetting = JsonUtility.ToJson(saveData);
+
+        File.WriteAllText(saveFilePath, savePlayerSetting);
+
+        Debug.Log("Datos guardados.");
+        
+    }
+    
+}
